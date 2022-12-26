91650015238f450e76b3ccae8656b81eeae9c9d0
ediane <edianeunb@gmail.com>
2022-12-26 10:20:27 -0300

    login screen

--- src/RoutersComponents.js
+++ src/RoutersComponents.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from '../src/pages/Login'
-import Cadastro from '../src/pages/Cadastro'
-import Planos from '../src/pages/Planos'
-import Plano from '../src/pages/Plano'
+// import Cadastro from '../src/pages/Cadastro'
+// import Planos from '../src/pages/Planos'
+// import Plano from '../src/pages/Plano'
 
 function RoutersComponents() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/sign-up" element={<Cadastro />} />
+        {/* <Route path="/sign-up" element={<Cadastro />} />
         <Route path="/subscriptions" element={<Planos />} />
-        <Route path="/subscriptions/:planoId" element={<Plano />} />
+        <Route path="/subscriptions/:planoId" element={<Plano />} /> */}
       </Routes>
     </Router>
   )
